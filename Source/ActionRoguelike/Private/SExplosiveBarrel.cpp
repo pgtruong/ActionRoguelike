8e97801b613fe1896f7d47e5ca0567f1dd263537
Peter Truong <petergtruong@gmail.com>
2022-11-07 21:39:00 -0800

    Added SExplosiveBarrel
    - Added an explosive barrel that will do a radial force when hit by the magic projectile to simulated actors.

---
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComp");
+	StaticMeshComp->SetCollisionProfileName("PhysicsActor");
+	StaticMeshComp->SetSimulatePhysics(true);
+	StaticMeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnBarrelHit);
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>("RadialForceComp");
+	RadialForceComp->SetupAttachment(StaticMeshComp);
+	RadialForceComp->SetAutoActivate(false);
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->Radius = 1000.0f;
+	RadialForceComp->ImpulseStrength = 2000.0f;
+}
+
+void ASExplosiveBarrel::OnBarrelHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComp->FireImpulse();
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
